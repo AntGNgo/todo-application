faf8940de07f2a5412c56d7b1e9c2ec3f3c8fa9a
AntGNgo <anthony.g.ngo@gmail.com>
2021-01-12 13:20:34 -0800

    desktop filters complete, WIP mobile filters and list count

--- app2.js
+++ app2.js
@@ -1,8 +1,8 @@
 (() => {
     const list = document.querySelector('.list'),
-    input = document.querySelector('.create-todo__input'),
-    itemsLeft = document.querySelector('.items-left')
-    listLength = 0
+    input = document.querySelector('.create-todo__input')
+    
+    let listLength = 0
 
     input.addEventListener('keydown', (e) => {
         if(e.keyCode === 13) {
@@ -17,7 +17,13 @@
 
     list.addEventListener('click', (e) => {
         let target = e.target
-        target.classList.toggle('completed')
+
+        if (!target.classList.contains('completed')) {
+            target.classList.add('completed')
+        } else {
+            target.classList.remove('completed')
+        }
+        editItemCount()
         store()
     })
 
@@ -37,11 +43,34 @@
     }
 
     const getState = () => {
-        const state = localStorage.items || []
-        list.innerHTML = state
+        return localStorage.items || []
     }
 
-    getState()
+    const itemCount = () => {
+        const itemsLeft = document.querySelector('.items-left')
+        listLength === 1 ? itemsLeft.innerHTML = `1 item left` :  itemsLeft.innerHTML = `${listLength} items left`
+    }
+
+    const editItemCount = () => {
+        let children = list.children
+
+        for(let i = 0; i < children.length; i++) {
+            let child = children[i]
+            if(!child.classList.contains('completed')) {
+                listLength++
+            }
+        }
+
+        itemCount()
+    }
+
+    const renderState = () => {
+        list.innerHTML = getState()
+        listLength = list.children.length
+        itemCount()
+    }
+
+    renderState()
 
     // Change Theme
 
@@ -55,6 +84,76 @@
         } else {
             toggleImg.src = './images/icon-sun.svg'
         }
+    })
+
+
+    // Filters
+
+    const showAll = document.querySelector('.show-all')
+    const showActive = document.querySelector('.show-active')
+    const showCompleted = document.querySelector('.show-completed')
+
+    const viewReset = () => {
+        let children = list.children
+
+        for(let i = 0; i < children.length; i++) {
+            let child = children[i]
+            child.style.display = "flex"
+        }
+    }
+
+    showAll.addEventListener('click', () => {
+        if(!showAll.classList.contains('selected')) {
+            showAll.classList.add('selected')
+            showActive.classList.remove('selected')
+            showCompleted.classList.remove('selected')
+        }
+        viewReset()
+    })
+
+    
+    showActive.addEventListener('click', () => {
+        if(!showActive.classList.contains('selected')) {
+            showActive.classList.add('selected')
+            showAll.classList.remove('selected')
+            showCompleted.classList.remove('selected')
+        }
+
+        viewReset()
+
+        let children = list.children
+
+        for(let i = 0; i < children.length; i++) {
+            let child = children[i]
+            if(child.classList.contains('completed')) {
+                console.log(child)
+                child.style.display = 'none'
+            }
+        }
+        
+    })
+
+    
+    showCompleted.addEventListener('click', () => {
+        if(!showCompleted.classList.contains('selected')) {
+            showCompleted.classList.add('selected')
+            showAll.classList.remove('selected')
+            showActive.classList.remove('selected')
+        }
+
+        viewReset()
+        
+        let children = list.children
+
+        for(let i = 0; i < children.length; i++) {
+            let child = children[i]
+            
+            if(!child.classList.contains('completed')) {
+                console.log(child)
+                child.style.display = 'none'
+            }
+        }
+        
     })
 
 })()
